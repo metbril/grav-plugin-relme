af3780009713f752465dd060ba4b9047750b2f61
Robert van Bregt <robert@robertvanbregt.nl>
2018-08-12 07:07:32 +0200

    Remove generated sample code

--- relme.php
+++ relme.php
@@ -39,7 +39,6 @@ class RelMePlugin extends Plugin
 
         // Enable the main event we are interested in
         $this->enable([
-            'onPageContentRaw' => ['onPageContentRaw', 0]
         ]);
     }
 
@@ -51,13 +50,5 @@ class RelMePlugin extends Plugin
      */
     public function onPageContentRaw(Event $e)
     {
-        // Get a variable from the plugin configuration
-        $text = $this->grav['config']->get('plugins.relme.text_var');
-
-        // Get the current raw content
-        $content = $e['page']->getRawContent();
-
-        // Prepend the output with the custom text and set back on the page
-        $e['page']->setRawContent($text . "\n\n" . $content);
     }
 }
